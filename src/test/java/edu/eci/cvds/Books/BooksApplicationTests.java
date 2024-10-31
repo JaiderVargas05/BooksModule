7034d24d7c5084e1f7f3386f771d9a493912f861
JaiderVargas05 <Jaider.vargas-n@mail.escuelaing.edu.co>
2024-10-31 13:55:30 -0500

    Upload Project base

---
+++ src/test/java/edu/eci/cvds/Books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.Books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
