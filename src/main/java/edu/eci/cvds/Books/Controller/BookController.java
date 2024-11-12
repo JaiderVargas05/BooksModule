b5dad5d9028216f462f42b03b926bb58acdaefca
JaiderVargas05 <Jaider.vargas-n@mail.escuelaing.edu.co>
2024-11-12 08:28:02 -0500

    cambio tamaño columna codeBar

--- src/main/java/edu/eci/cvds/Books/Controller/BookController.java
+++ src/main/java/edu/eci/cvds/Books/Controller/BookController.java
@@ -9,6 +9,7 @@ import edu.eci.cvds.Books.Service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
